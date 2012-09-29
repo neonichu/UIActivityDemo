aeb17c6760544b9c12ce45c621c209161457816c
Boris Bügling <boris@icculus.org>
2012-09-29 18:35:41 +0200

    Initial Commit

---
+++ UIActivityDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIActivityDemo
+//
+//  Created by Boris Buegling on 29.09.12.
+//  Copyright (c) 2012 Boris Buegling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
